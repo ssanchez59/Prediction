728453d66c6845a80e6643d5b85afa7e7721911a
ssanchez59 <s.sanchez59@uniandes.edu.co>
2019-04-27 14:14:27 -0500

    ignore config

--- server/config/keys.js
+++
@@ -1,8 +0,0 @@
-// prod.js - production keys here!!
-module.exports = {
-    mongoURI: 'mongodb+srv://dbUser:dbUserPassword@tensor-islzu.mongodb.net/test?retryWrites=true',
-    googleClientID: '1043277571775-j9pnm9anr92qr5ocvo66l2eolvghb9pf.apps.googleusercontent.com',
-    googleClientSecret: 'zE5lTeedgFoMcYAYJNnaUEOF',
-    cookieKey: 'dajsfkasdkfaksndfaskjdfaksdf'
-  };
-  
